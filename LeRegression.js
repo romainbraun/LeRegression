1a032a8d58bee65448ce49f767a8e21e5f4be302
Romain Braun <romain@oldstlabs.com>
2016-06-14 15:07:15 +0100

    reorganising files

--- LeRegression.js
+++ LeRegression.js
@@ -1,26 +1,18 @@
 #!/usr/bin/env node
 
-var s3                = require('s3'),
-    fs                = require('fs'),
+var s3Sync            = require('./lib/s3Sync'),
+    folders           = require('./lib/folders'),
+    image             = require('./lib/image'),
+    html              = require('./lib/html'),
+    github            = require('./lib/github'),
     path              = require('path'),
-    exec              = require('child_process').exec,
+    fs                = require('fs'),
     https             = require('https'),
     rimraf            = require('rimraf'),
     dirTree           = require('directory-tree'),
-    execFile          = require('child_process').execFile,
-    handlebars        = require('handlebars'),
     program           = require('commander'),
-    refPath           = '/tmp/leregression/reference/',
-    regPath           = '/tmp/leregression/regression/',
-    comparePath       = '/tmp/leregression/compare',
     isRemoteReference = false,
-    githubToken,
-    commitHash,
-    threshold,
-    s3config,
-    options,
-    config,
-    client;
+    config;
 
 program
   .option('-c, --config-file [config-file]', 'Configuration JSON')
@@ -31,179 +23,102 @@ program
   .option('-r, --reset-reference', 'The script will only create reference files')
   .parse(process.argv);
 
-config = JSON.parse(fs.readFileSync(program.configFile, 'utf8'));
-
-threshold = config.threshold;
-s3config = config.s3config;
-
-s3config.client.s3Options = {
-  accessKeyId: program.accessKeyId,
-  secretAccessKey: program.secretAccessKey
-};
-
-commitHash  = program.commitHash;
-githubToken = program.githubToken;
-
-options = {
-  host: 'api.github.com',
-  path: '/repos/' + config.repository + '/statuses/' + commitHash,
-  headers: {
-    'User-Agent': s3config.bucket.name,
-    'Authorization': 'token ' + githubToken
-  }
-};
-
-client = s3.createClient(s3config.client);
-
 init();
 
-/**
- * STEPS:
- * 1. ✔︎ Check for reference folder (non empty s3 bucket)
- * 2. ✔︎ Download it if there is one
- * 3. ✔︎ Delete local regression SS folder if it exists
- * 4. ✔︎ Run regression tests on Browserstack
- * 5. ✔︎ Upload regression folder
- * 6. ✔︎ If there was no reference folder, duplicate regression one on s3
- * 7. ✔︎ Remove Compare folder if it exists
- * 8. ✔︎ Compare if there is a reference folder
- * 9. ✔︎ Upload compare folder
- * 10. ✔︎ Build HTML file
- * 11. Upload HTML
- * 12. ✔︎ Do some kind of magic to tell github or something
- * 13. If we're on master or if it's been approved idk, make regression folder the new reference folder on s3
- */
-
 function init() {
-  rimraf('/tmp/leregression/', function() {
-    fs.mkdirSync('/tmp/leregression/');
-    fs.createReadStream(config.sitemap).pipe(fs.createWriteStream('/tmp/leregression/sitemap.json'));
-    fs.createReadStream(path.join(__dirname, 'spec/regression.spec.js')).pipe(fs.createWriteStream('/tmp/leregression/regression.spec.js'));
+  config = JSON.parse(fs.readFileSync(program.configFile, 'utf8'));
+
+  s3Sync.createClient(config.s3config, program.accessKeyId, program.secretAccessKey);
+
+  github.setup({
+    'repository': config.repository,
+    'commitHash': program.commitHash,
+    'bucketName': config.s3config.bucket.name,
+    'githubToken': program.githubToken
+  });
+
+  folders.createSafely('/tmp/leregression/', function() {
+    folders.move(config.sitemap, '/tmp/leregression/sitemap.json');
+    folders.move(path.join(__dirname, 'spec/regression.spec.js'), '/tmp/leregression/regression.spec.js');
+
     clean();
   });
 }
 
 function clean() {
   console.log('Cleaning existing files');
-  rimraf(refPath, function() {
+  rimraf(folders.refPath, function() {
     if (!program.resetReference) {
       console.log('Downloading existing reference if there is one');
       downloadRemoteReference();
     }
   });
-  rimraf(regPath, function() {
+  rimraf(folders.regPath, function() {
     takeScreenshots();
   });
 }
 
-/**
- * STEP 2
- */
 function downloadRemoteReference() {
   var params = {
-    localDir: refPath,
+    localDir: folders.refPath,
 
     s3Params: {
-      Bucket: s3config.bucket.name,
+      Bucket: config.s3config.bucket.name,
       Prefix: "reference/"
-    },
+    }
+  };
 
-
-  },
-  downloader = client.downloadDir(params);
-
-  downloader.on('error', function(err) {
-    console.error("unable to sync:", err.stack);
-  });
-  downloader.on('end', function() {
+  s3Sync.download(params, function() {
     console.log("done downloading reference");
   });
 }
 
-/**
- * STEP 4
- */
 function takeScreenshots() {
-  var command = '';
-
-  console.log('Taking screenshots...');
-  for (var i = 0; i < config.capabilities.length; i++) {
-    var capability = config.capabilities[i];
-    command += (process.env.PROTRACTOR_BIN || 'protractor') + ' ' + capability + ' & ';
-  }
-  command += 'wait';
-
-  var child = exec(command,
-    function (error, stdout, stderr) {
-      console.log('Done!');
-      checkForLocalReference();
-      if (error !== null) {
-        console.log('exec error: ' + error);
-      }
+  image.takeScreenshots(config.capabilities, function() {
+    checkForLocalReference();
   });
 }
 
-/**
- * STEP 5
- */
 function uploadRegressionDirectory() {
   console.log('uploading screenshots...');
+
   var params = {
-    localDir: regPath,
+    localDir: folders.regPath,
 
     s3Params: {
-      Bucket: s3config.bucket.name,
+      Bucket: config.s3config.bucket.name,
       Prefix: "regression/",
       ACL: 'public-read'
-    },
-
-
+    }
   };
-  var uploader = client.uploadDir(params);
-  uploader.on('error', function(err) {
-    console.error("unable to sync:", err.stack);
-  });
-  uploader.on('end', function() {
-    console.log("done uploading");
-    compareScreenshots();
-  });
+
+  s3Sync.upload(params);
 }
 
-/**
- * STEP 6
- */
 function uploadReferenceDirectory() {
   console.log('uploading reference...');
 
   var params = {
-    localDir: regPath,
+    localDir: folders.regPath,
 
     s3Params: {
-      Bucket: s3config.bucket.name,
+      Bucket: config.s3config.bucket.name,
       Prefix: "reference/",
       ACL: 'public-read'
-    },
-
-
+    }
   };
-  var uploader = client.uploadDir(params);
-  uploader.on('error', function(err) {
-    console.error("unable to sync:", err.stack);
-  });
-  uploader.on('end', function() {
-    console.log("done uploading");
+
+  s3Sync.upload(params, function() {
     process.exit();
   });
 }
 
-/**
- * STEP 8.1
- */
 function checkForLocalReference() {
-  fs.exists(refPath, function(exists) {
+  fs.exists(folders.refPath, function(exists) {
     if (exists) {
       console.log('Reference found.');
       uploadRegressionDirectory();
+      compareScreenshots();
     } else {
       console.log('No reference found.');
       uploadReferenceDirectory();
@@ -211,134 +126,42 @@ function checkForLocalReference() {
   });
 }
 
-/**
- * STEP 8.2
- */
 function compareScreenshots() {
-  var fileStructure = dirTree(refPath),
-      child,
-      callbackCount = 0,
-      count = 0;
-
-  function checkForCompletion() {
-    if (callbackCount && callbackCount === count) {
-      console.log('Done comparing.');
-      uploadComparedFiles();
-      buildHTMLFile();
-    } else {
-      setTimeout(checkForCompletion, 1000);
-    }
-  }
-
-  function computeResult(resolution, image) {
-    return function (error, stdout, stderr) {
-      if (stderr < threshold) {
-        rimraf(path.join(comparePath, resolution, image), function() {
-          console.log('✔︎ No regression');
-          callbackCount++;
-        });
-      } else {
-        console.log('✘ Regression detected!');
-        callbackCount++;
-      }
-    };
-  }
-
-  console.log('comparing screenshots...');
-
-  rimraf(comparePath, function() {
-    fs.mkdirSync(comparePath);
-
-    for (var i = 0; i < fileStructure.children.length; i++) {
-      var resolution = fileStructure.children[i].name;
-
-      fs.mkdirSync(path.join(comparePath, resolution));
-
-      for (var j = 0; j < fileStructure.children[i].children.length; j++) {
-        var image = fileStructure.children[i].children[j].name;
-
-        count++;
-
-        exec('compare -metric AE -fuzz 20% ' +
-             path.join(refPath, resolution, image) + ' ' +
-             path.join(regPath, resolution, image) + ' ' +
-             path.join(comparePath, resolution, image),
-             computeResult(resolution, image));
-      }
-    }
-
-    checkForCompletion();
-  });
+  image.compareScreenshots(
+    folders.refPath, 
+    folders.regPath, 
+    folders.comparePath, 
+    config.threshold,
+    buildHTMLFile
+  );
 }
 
-/**
- * STEP 9
- */
-function uploadComparedFiles() {
-  console.log('uploading compared files...');
-  var params = {
-    localDir: comparePath,
-
-    s3Params: {
-      Bucket: s3config.bucket.name,
-      Prefix: "compare/",
-      ACL: 'public-read'
-    },
-  };
-  var uploader = client.uploadDir(params);
-  uploader.on('error', function(err) {
-    console.error("unable to sync:", err.stack);
-  });
-  uploader.on('end', function() {
-    console.log("done uploading compared files");
-
-  });
-}
-
-/**
- * STEP 10 or something
- */
 function buildHTMLFile() {
-  var fileStructure = dirTree(comparePath);
-
-
-  if (!fileStructure || !fileStructure.children.length) {
-    console.log('Regression checks all passed.');
-    process.exit();
-  }
-
-  console.log('building HTML file');
-
-  fs.readFile(path.join(__dirname, 'view/template.html'), function(err, data){
-    if (!err) {
-      // make the buffer into a string
-      var source = data.toString();
-      // call the render function
-      renderToString(source, fileStructure);
-    } else {
-      // handle file read error
-    }
-  });
-
-  // this will be called after the file is read
-  function renderToString(source, data) {
-    var template = handlebars.compile(source);
-
-    data.hash  = commitHash;
-    data.token = githubToken;
-    data.path = options.path;
-
-    var outputString = template(data);
-
-    fs.writeFile("/tmp/rendered.html", outputString, function(err) {
-      if(err) {
-          return console.log(err);
-      }
-
+  uploadComparedFiles();
+  html.buildHTMLFile(
+    folders.comparePath, 
+    program.commitHash,
+    program.githubToken,
+    github.getPath(),
+    function() {
       uploadHTML();
     });
-  }
+}
 
+function uploadComparedFiles() {
+  console.log('uploading compared files...');
+
+  var params = {
+    localDir: folders.comparePath,
+
+    s3Params: {
+      Bucket: config.s3config.bucket.name,
+      Prefix: "compare/",
+      ACL: 'public-read'
+    }
+  };
+
+  s3Sync.upload(params);
 }
 
 function uploadHTML() {
@@ -346,89 +169,14 @@ function uploadHTML() {
     localFile: "/tmp/rendered.html",
 
     s3Params: {
-      Bucket: s3config.bucket.name,
-      Key: 'compare'+ commitHash +'.html',
+      Bucket: config.s3config.bucket.name,
+      Key: 'compare'+ program.commitHash +'.html',
       ACL: 'public-read'
     },
   };
 
-  var uploader = client.uploadFile(params);
-
-  uploader.on('error', function(err) {
-    console.error("unable to upload:", err.stack);
+  s3Sync.uploadFile(params, function() {
+    github.postGitStatus('pending', program.commitHash, config.s3config.bucket.name);
+    github.getGitStatus();
   });
-
-  uploader.on('end', function() {
-    console.log("done uploading HTML file.");
-    postGitStatus('pending');
-    getGitStatus();
-  });
-}
-
-function getGitStatus() {
-  var callback = function(response) {
-    var output = '';
-
-    response.on('data', function(data) {
-      output += data;
-    });
-
-    response.on('end', function() {
-      var data = JSON.parse(output);
-
-      for (var i = 0; i < data.length; i++) {
-        if (
-          data[i].status.context === 'LeRegression' &&
-          data[i].status.state !== 'pending'
-        ) {
-          postGitStatus(data[i].status.state);
-        } else {
-          console.log('Awaiting user input from Github');
-          doSetTimeout();
-        }
-      }
-    });
-  };
-
-  https.request(options, callback).end();
-}
-
-function doSetTimeout() {
-  setTimeout(function() {
-    getGitStatus();
-  }, 3600);
-}
-
-function postGitStatus(status) {
-  options.method = 'POST';
-
-  var request = https.request(options);
-
-  var url = 'http://s3-eu-west-1.amazonaws.com/leregression/compare' + commitHash +
-            '.html';
-
-  var content = {
-    'state': status,
-    'target_url': url,
-    'context': 'LeRegression'
-  };
-
-  switch (status) {
-
-    case 'success':
-      content.description = 'Regression tests passed!';
-      break;
-
-    case 'pending':
-      content.description = 'Awaiting user input!';
-      break;
-
-    case 'failure':
-      content.description = 'Regression tests failed!';
-      break;
-
-  }
-
-  request.write(JSON.stringify(content));
-  request.end();
 }
